605914f0079427009c9cd8f7f48b7fa2e16b1728
Zwhea <theawang@yahoo.com>
2023-02-23 20:04:13 +0100

    link moves

--- sources/events/motions.c
+++ sources/events/motions.c
@@ -6,7 +6,7 @@
 /*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 13:59:53 by wangthea          #+#    #+#             */
-/*   Updated: 2023/02/23 17:07:22 by wangthea         ###   ########.fr       */
+/*   Updated: 2023/02/23 19:47:39 by wangthea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ static void	motion_left(t_game *g)
 		g->map.map[i][j] = space;
 		display_image(g, g->txtr.items.grass, i, j);
 		g->map.map[i][j - 1] = player;
-		g->map.pos_x_player = j--;
-		display_image(g, g->txtr.player.link, i, j);
+		g->map.pos_x_player--;
+		display_image(g, g->txtr.player.link, i, j - 1);
 		g->map.moves++;
 		ft_dprintf(1, BLUE"moves count ="END" %d\n", g->map.moves);
 	}
@@ -43,8 +43,46 @@ static void	motion_right(t_game *g)
 		g->map.map[i][j] = space;
 		display_image(g, g->txtr.items.grass, i, j);
 		g->map.map[i][j + 1] = player;
-		g->map.pos_x_player = j++;
-		display_image(g, g->txtr.player.link, i, j);
+		g->map.pos_x_player++;
+		display_image(g, g->txtr.player.link, i, j + 1);
+		g->map.moves++;
+		ft_dprintf(1, BLUE"moves count ="END" %d\n", g->map.moves);
+	}
+}
+
+static void	motion_top(t_game *g)
+{
+	int	i;
+	int	j;
+
+	i = g->map.pos_y_player;
+	j = g->map.pos_x_player;
+	if (g->map.map[i - 1][j] != wall)
+	{
+		g->map.map[i][j] = space;
+		display_image(g, g->txtr.items.grass, i, j);
+		g->map.map[i - 1][j] = player;
+		g->map.pos_y_player--;
+		display_image(g, g->txtr.player.link, i - 1, j);
+		g->map.moves++;
+		ft_dprintf(1, BLUE"moves count ="END" %d\n", g->map.moves);
+	}
+}
+
+static void	motion_bot(t_game *g)
+{
+	int	i;
+	int	j;
+
+	i = g->map.pos_y_player;
+	j = g->map.pos_x_player;
+	if (g->map.map[i + 1][j] != wall)
+	{
+		g->map.map[i][j] = space;
+		display_image(g, g->txtr.items.grass, i, j);
+		g->map.map[i + 1][j] = player;
+		g->map.pos_y_player++;
+		display_image(g, g->txtr.player.link, i + 1, j);
 		g->map.moves++;
 		ft_dprintf(1, BLUE"moves count ="END" %d\n", g->map.moves);
 	}
@@ -58,22 +96,9 @@ int	key_press(t_keycode key_code, t_game *game)
 		motion_left(game);
 	else if (key_code == d_key || key_code == right_key)
 		motion_right(game);
+	if (key_code == w_key || key_code == up_key)
+		motion_top(game);
+	if (key_code == s_key || key_code == down_key)
+		motion_bot(game);
 	return (0);
-	// if (key_code == w_key || key_code == up_key)
-		// motion_top(game);
-	// if (key_code == s_key || key_code == down_key)
-		// motion_bot(game);
 }
-
-
-
-// void	motion_top(t_game *g)
-// {
-	// 
-// }
-// 
-// void	motion_bot(t_game *g)
-// {
-// 
-// }
-// 
